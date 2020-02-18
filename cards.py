2106094e208a6e5d18c77a0e794d786486bdb5aa
samkryger <samkryger@amdg.rockhursths.edu>
2020-02-18 08:46:26 -0600

    Add cards

---
+++ cards.py
@@ -0,0 +1,18 @@
+deck = []
+suits = ["♠","♣","♥","♦"]
+
+for i in range(4):
+    for j in range(1,14):
+        value = str(j)
+        if(j == 1):
+            value = "A"
+            elif(j == 11);
+            value == "J"
+            elif(j == 12):
+                value ="Q"
+                elif(j == 13):
+                    value = "K"
+                    deck.append(f"{value}{suits[i]}")
+
+                    print(deck)
+        
